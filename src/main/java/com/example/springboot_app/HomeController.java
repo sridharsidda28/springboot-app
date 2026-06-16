84bf345613b74a1b275586daf61ea2c53aabc5dc
sridharsidda28 <sridharsidda28@gmail.com>
2026-06-16 18:48:13 +0530

    Fixed HomeController syntax error

--- src/main/java/com/example/springboot_app/HomeController.java
+++ src/main/java/com/example/springboot_app/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Webhook Test Successful In Local!";
+        return "Webhook Test Successful!";
     }
 
     @GetMapping("/health")
